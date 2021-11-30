7057af7c81d407273551f52924ba4b5e4e8f90d6
Edwin-Baranov <edwin.baranov@gmail.com>
2021-11-29 17:35:35 -0800

    cleanup

--- server/queries.js
+++ server/queries.js
@@ -8,7 +8,7 @@ const pool = new Pool({
 })
 
 module.exports = {
-  getQuestions: (product_id, page = 1, count = 5, callback) => { //TODO: Return answers to questions
+  getQuestions: (product_id, page = 1, count = 5, callback) => {
     pool.query(
       `SELECT
         question_id,
@@ -51,14 +51,14 @@ module.exports = {
       })
   },
 
-  postQuestion: (body, name, email, product_id, callback) => {  //TODO: utilize the no null functionality of the table
+  postQuestion: (body, name, email, product_id, callback) => {
     pool.query('INSERT INTO questions (question_body, asker_name, asker_email, product_id) VALUES ($1, $2, $3, $4)', [body, name, email, product_id],
       (err, results) => {
         err ? callback(err) : callback(err, results.rows);
       })
   },
 
-  postAnswer: (body = '', name = '', email = '', photos = '', question_id = '', callback) => {  //TODO: utilize the no null functionality of the table and photos
+  postAnswer: (body = '', name = '', email = '', photos = '', question_id = '', callback) => {
     pool.query('INSERT INTO answers (body, answerer_name, answerer_email, question_id) VALUES ($1, $2, $3, $4)', [body, name, email, question_id],
       (err, results) => {
         err ? callback(err) : callback(err, results.rows);
@@ -79,48 +79,5 @@ module.exports = {
 
   markAnswerReport: (answer_id, callback) => {
     pool.query(`update answers set reported = true where answer_id = ${answer_id}`, callback);
-  },
-
-  test: (question_id, callback) => {
-    pool.query(
-      `SELECT
-         json_object_agg(
-          answer_id,
-          json_build_object(
-            'id', answer_id,
-            'body', body,
-            'date', date,
-            'answerer_name', answerer_name,
-            'helpfulness', helpfulness,
-            'photos', 'test'
-          )
-        ) AS answers FROM answers WHERE question_id = $1 AND reported = false
-       `,
-      [question_id],
-      (err, result) => {
-        err ? callback(err) : callback(err, result.rows);
-      });
   }
-
-  /*SELECT
-  id,
-  to_char(in_time, 'Dy MM.DD.YY') AS inDate,
-  to_char(out_time, 'Dy MM.DD.YY') AS outDate,
-  to_char( in_time, 'HH:MI am' ) AS inTime,
-  to_char( out_time, 'HH:MI am' ) AS outTime,
-  to_char( total_time, 'HH24h MIm SSs' ) AS totalTime,
-  in_time AS inTimeObj,
-  rate,
-  value,
-  (SELECT array(
-      SELECT t2.tag_name
-      FROM logtags t1
-      LEFT JOIN tags t2
-      ON t1.tag_id=t2.tag_id
-      WHERE log_id=timelogs.id
-      GROUP BY t1.tag_id, t2.tag_name
-  ) AS TAGS)
-FROM timelogs
-WHERE user_id=$1;*/
-
 }
